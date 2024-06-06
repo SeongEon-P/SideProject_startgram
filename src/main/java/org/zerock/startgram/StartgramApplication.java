38b4dd9d23275aec4f4382977c602ca94ed88b44
박성언 <tjddjs909@naver.com>
2024-06-06 10:33:25 +0900

    first commit

---
+++ src/main/java/org/zerock/startgram/StartgramApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.startgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/startgram/StartgramApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.startgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
